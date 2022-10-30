bf142cc82edb4186e1a1e8cd9c46a0f2dace8068
omar-moquete <moqueteperez@outlook.com>
2022-10-30 13:24:38 -0400

    updated comments

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,15 @@ const router = createBrowserRouter(
     >
       <Route path="/" element={<WelcomePage />} />
       <Route path="/blog" element={<BlogLayout />}>
-        {/* When this route is executed, first the loader function is called, then <BlogPostsPage> is rendered. The returned value from the loader function is made available when <BlogPostsPage> is being rendered via the useLoaderData() hook from RRv6.4.  If the loader function returns a promise, the element will wait for this promise to be settled before rendering.*/}
+        {/* When this route is executed, first the loader function is called, then <BlogPostsPage> is rendered. The returned value from the loader function is made available when <BlogPostsPage> is being rendered via the useLoaderData() hook from RRv6.4. 
+        
+        Important note: If the loader function returns a promise, the data that will be returned from useLoaderData() is the data to which the promise resolves to. In other words, the result of calling the loader function is awaited by RRv6.4 before rendering by default.*/}
 
         {/* "index" means that this route will become active when "/blog" is active */}
 
         {/* "errorElement": When exceptions are thrown in loaders, actions, or component rendering, instead of the normal render path for your Routes (<Route element>), the error path will be rendered (<Route error Element>) and the error made available with useRouteError. */}
+
+        {/* The loader can be seen as the GET part of a HTTP request. It can make the API call and make the data available in the component that will be rendered when this data arrives. */}
         <Route index element={<BlogPostsPage />} loader={blogPostsLoader} />
 
         <Route path=":id" element={<PostDetailPage />} />
--- src/pages/BlogPosts.jsx
+++ src/pages/BlogPosts.jsx
@@ -7,7 +7,6 @@ import { getPosts } from "../util/api";
 
 function BlogPostsPage() {
   const loaderData = useLoaderData();
-  console.log("loaderData", loaderData);
   return (
     <>
       <h1>Our Blog Posts</h1>
@@ -20,11 +19,11 @@ export default BlogPostsPage;
 
 // Create separate function which returns the data that should be available in our <BlogPostsPage> component.
 export const loader = function ({ request, params }) {
-  console.log("request", request);
   // getPosts generates and sends an HTTP request to fetch some posts, checks if the fetch was successful, if not, it throws a custom error object, and if fetching the data was successful it returns a promise that eventually resolves to the JSON data that's part of the response.
 
   // This promise returned by getPosts() is the value returned by this function.
 
   // This alone does not do anything, but now this loader function can be registered on our route definition (in App.jsx).
+
   return getPosts();
 };
